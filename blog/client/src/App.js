7388b36395e0135d1b7d17f657ca805b1ed01ad7
mansuuk <manasdange@gmail.com>
2020-07-14 19:48:55 +0530

    added moderation sevice

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -3,11 +3,13 @@ import PostCreate from './PostCreate';
 import PostList from './PostList';
 
 export default () => {
-    return <div className="container">
-        <h1>Create Post</h1>
-        <PostCreate />
-        <hr />
-        <h1>Post</h1>
-        <PostList />
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
+  );
 };
--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -2,24 +2,31 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export default () => {
-    const [title, setTitle] = useState('');
-    const onSubmit = async (event) => {
-        event.preventDefault();
+  const [title, setTitle] = useState('');
 
-        await axios.post('http://localhost:4000/posts', {
-            title
-        }); 
-        
-        setTitle('');
-    };
-    return(<div>
-        <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <label>Title</label>
-                <input value={title} onChange ={e => setTitle(e.target.value)} className="form-control"></input>
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
-    )
+  );
 };
